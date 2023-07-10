e2715080abe47fbb19951e9827a91fd5f484512d
Patrick Monaghan <0x5f.manpat@gmail.com>
2023-07-10 23:08:20 +0100

    update for addition of toybox_gfx

--- src/main.rs
+++ src/main.rs
@@ -5,46 +5,40 @@ use toybox::host::Host;
 fn main() -> Result<(), Box<dyn Error>> {
 	std::env::set_var("RUST_BACKTRACE", "1");
 
-	let host = Host::create()?;
+	let host = Host::create("playbox")?;
+	host.install_default_error_handler();
+
 	let Host{ event_loop, gl_state: gl, surface, gl_context, .. } = host;
+	let gfx_core = gfx::Core::new(surface, gl_context, gl);
 
 	
 	event_loop.run(move |event, _, control_flow| {
 		use host::winit::event::*;
-		use host::glutin::surface::GlSurface;
 
-		control_flow.set_wait();
+		control_flow.set_poll();
 
 		match event {
 			Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => control_flow.set_exit(),
-
 
 			Event::DeviceEvent { event: DeviceEvent::Key(KeyboardInput{ virtual_keycode: Some(VirtualKeyCode::Escape), .. }), .. } => {
 				control_flow.set_exit();
 			}
 
 			Event::MainEventsCleared => unsafe {
-				gl.ClearColor(1.0, 0.0, 1.0, 1.0);
-				gl.Clear(gl::COLOR_BUFFER_BIT);
+				gfx_core.gl.ClearColor(0.5, 0.5, 0.5, 1.0);
+				gfx_core.gl.Clear(gl::COLOR_BUFFER_BIT);
 
 
-				surface.swap_buffers(&gl_context).unwrap();
+				gfx_core.finalize_frame();
 			}
+
+			// Event::WindowEvent { event: WindowEvent::Resized(physical_size), .. } => {
+			// 	main_loop.resize(Vec2i::new(physical_size.width as i32, physical_size.height as i32));
+			// }
 
 
 			_ => (),
 		}
 	})
-
-	// let mut engine = toybox::Engine::new("playbox")?;
-
-	// 'main: loop {
-	// 	engine.process_events();
-	// 	if engine.should_quit() || engine.input.raw_state.active_buttons.contains(&input::Keycode::Escape.into()) {
-	// 		break 'main
-	// 	}
-
-	// 	engine.end_frame();
-	// }
 }
 
