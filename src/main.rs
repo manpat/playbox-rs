45403de5765ef1742ba850503c7a487d06cd2cd5
Patrick Monaghan <0x5f.manpat@gmail.com>
2023-07-10 21:55:26 +0100

    first step after rewrite begun

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,50 @@
 use toybox::prelude::*;
 
+use toybox::host::Host;
+
 fn main() -> Result<(), Box<dyn Error>> {
 	std::env::set_var("RUST_BACKTRACE", "1");
 
-	let mut engine = toybox::Engine::new("playbox")?;
+	let host = Host::create()?;
+	let Host{ event_loop, gl_state: gl, surface, gl_context, .. } = host;
 
-	'main: loop {
-		engine.process_events();
-		if engine.should_quit() || engine.input.raw_state.active_buttons.contains(&input::Keycode::Escape.into()) {
-			break 'main
+	
+	event_loop.run(move |event, _, control_flow| {
+		use host::winit::event::*;
+		use host::glutin::surface::GlSurface;
+
+		control_flow.set_wait();
+
+		match event {
+			Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => control_flow.set_exit(),
+
+
+			Event::DeviceEvent { event: DeviceEvent::Key(KeyboardInput{ virtual_keycode: Some(VirtualKeyCode::Escape), .. }), .. } => {
+				control_flow.set_exit();
+			}
+
+			Event::MainEventsCleared => unsafe {
+				gl.ClearColor(1.0, 0.0, 1.0, 1.0);
+				gl.Clear(gl::COLOR_BUFFER_BIT);
+
+
+				surface.swap_buffers(&gl_context).unwrap();
+			}
+
+
+			_ => (),
 		}
+	})
 
-		engine.end_frame();
-	}
+	// let mut engine = toybox::Engine::new("playbox")?;
 
-	Ok(())
+	// 'main: loop {
+	// 	engine.process_events();
+	// 	if engine.should_quit() || engine.input.raw_state.active_buttons.contains(&input::Keycode::Escape.into()) {
+	// 		break 'main
+	// 	}
+
+	// 	engine.end_frame();
+	// }
 }
 
