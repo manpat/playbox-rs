26b744c440e3a19987b152522add415f2c9d6cc4
Patrick Monaghan <0x5f.manpat@gmail.com>
2023-07-12 23:49:12 +0100

    Experiment with markers

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,29 @@ impl toybox::App for App {
 		unsafe {
 			ctx.gfx_core.gl.ClearColor(0.5, 0.5, 0.5, 1.0);
 			ctx.gfx_core.gl.Clear(gl::COLOR_BUFFER_BIT);
+
+			let message = b"Hello\0";
+			let id = 1234;
+
+			ctx.gfx_core.gl.DebugMessageInsert(
+				gl::DEBUG_SOURCE_APPLICATION,
+				gl::DEBUG_TYPE_MARKER,
+				id,
+				gl::DEBUG_SEVERITY_NOTIFICATION,
+				message.len() as i32,
+				message.as_ptr() as *const _
+			);
+
+			let message = b"Goodbye\0";
+
+			ctx.gfx_core.gl.DebugMessageInsert(
+				gl::DEBUG_SOURCE_APPLICATION,
+				gl::DEBUG_TYPE_MARKER,
+				id,
+				gl::DEBUG_SEVERITY_HIGH,
+				message.len() as i32,
+				message.as_ptr() as *const _
+			);
 		}
 	}
 }
