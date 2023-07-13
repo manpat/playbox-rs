c08b98b5f83298b7ff717a65b494cba05977df8a
Patrick Monaghan <0x5f.manpat@gmail.com>
2023-07-13 22:53:37 +0100

    toybox changes +experiment with DSA framebuffer clearing

--- src/main.rs
+++ src/main.rs
@@ -12,32 +12,19 @@ struct App;
 
 impl toybox::App for App {
 	fn present(&mut self, ctx: &mut toybox::Context) {
+		ctx.gfx.core.push_debug_group("Wahoo");
+
 		unsafe {
-			ctx.gfx_core.gl.ClearColor(0.5, 0.5, 0.5, 1.0);
-			ctx.gfx_core.gl.Clear(gl::COLOR_BUFFER_BIT);
+			ctx.gfx.core.gl.ClearColor(0.5, 0.5, 0.5, 1.0);
+			// ctx.gfx.core.gl.Clear(gl::COLOR_BUFFER_BIT);
 
-			let message = b"Hello\0";
-			let id = 1234;
-
-			ctx.gfx_core.gl.DebugMessageInsert(
-				gl::DEBUG_SOURCE_APPLICATION,
-				gl::DEBUG_TYPE_MARKER,
-				id,
-				gl::DEBUG_SEVERITY_NOTIFICATION,
-				message.len() as i32,
-				message.as_ptr() as *const _
-			);
-
-			let message = b"Goodbye\0";
-
-			ctx.gfx_core.gl.DebugMessageInsert(
-				gl::DEBUG_SOURCE_APPLICATION,
-				gl::DEBUG_TYPE_MARKER,
-				id,
-				gl::DEBUG_SEVERITY_HIGH,
-				message.len() as i32,
-				message.as_ptr() as *const _
-			);
+			ctx.gfx.core.gl.ClearNamedFramebufferfv(0, gl::COLOR, 0, [0.5, 0.0, 0.5, 1.0].as_ptr());
 		}
+
+		ctx.gfx.core.debug_marker("Hello");
+
+		ctx.gfx.core.pop_debug_group();
+
+		ctx.gfx.core.debug_marker("Goodbye");
 	}
 }
