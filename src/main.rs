640abd8eb2b2c4e635da68328999f79aa62acc27
Patrick Monaghan <0x5f.manpat@gmail.com>
2023-07-18 00:45:39 +0100

    test frame encoder

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,30 @@ use toybox::*;
 fn main() -> anyhow::Result<()> {
 	std::env::set_var("RUST_BACKTRACE", "1");
 
-	toybox::run("playbox", |_| Ok(App))
+	toybox::run("playbox", App::new)
 }
 
 
 
 struct App;
 
+impl App {
+	fn new(ctx: &mut toybox::Context) -> anyhow::Result<App> {
+		ctx.gfx.frame_encoder.backbuffer_color([1.0, 0.5, 1.0]);
+
+		let mut group = ctx.gfx.frame_encoder.command_group("START");
+		group.debug_marker("FUCK");
+
+		Ok(App)
+	}
+}
+
 impl toybox::App for App {
 	fn present(&mut self, ctx: &mut toybox::Context) {
-		ctx.gfx.core.push_debug_group("Wahoo");
-
-		unsafe {
-			ctx.gfx.core.gl.ClearColor(0.5, 0.5, 0.5, 1.0);
-			// ctx.gfx.core.gl.Clear(gl::COLOR_BUFFER_BIT);
-
-			ctx.gfx.core.gl.ClearNamedFramebufferfv(0, gl::COLOR, 0, [0.5, 0.0, 0.5, 1.0].as_ptr());
-		}
-
-		ctx.gfx.core.debug_marker("Hello");
-
-		ctx.gfx.core.pop_debug_group();
-
-		ctx.gfx.core.debug_marker("Goodbye");
+		let mut group = ctx.gfx.frame_encoder.command_group("MY Group");
+		group.debug_marker("Group Time");
+		group.execute(|core, _rm| {
+			core.debug_marker("User Callback");
+		});
 	}
 }
