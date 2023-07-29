206f229bd5a55bba8241cdcf336cc53fc452ebc1
Patrick Monaghan <0x5f.manpat@gmail.com>
2023-07-29 16:15:31 +0100

    Make a start on draw command

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,6 @@ fn main() -> anyhow::Result<()> {
 struct App {
 	v_shader: gfx::resource_manager::shader::ShaderHandle,
 	f_shader: gfx::resource_manager::shader::ShaderHandle,
-
-	pipeline: gfx::core::ShaderPipelineName,
 }
 
 impl App {
@@ -22,19 +20,11 @@ impl App {
 		let mut group = ctx.gfx.frame_encoder.command_group("START");
 		group.debug_marker("FUCK");
 
-		unsafe {
-			let mut vao = 0;
-			ctx.gfx.core.gl.CreateVertexArrays(1, &mut vao);
-			ctx.gfx.core.gl.BindVertexArray(vao);
-		}
-
 		use gfx::resource_manager::shader::ShaderDef;
 
 		Ok(App {
 			v_shader: ctx.gfx.resource_manager.create_shader(ShaderDef::from("shaders/test.vs.glsl")?),
 			f_shader: ctx.gfx.resource_manager.create_shader(ShaderDef::from("shaders/test.fs.glsl")?),
-
-			pipeline: ctx.gfx.core.create_shader_pipeline(),
 		})
 	}
 }
@@ -44,19 +34,8 @@ impl toybox::App for App {
 		let mut group = ctx.gfx.frame_encoder.command_group("MY Group");
 		group.debug_marker("Group Time");
 
-		let pipeline = self.pipeline;
-		let v_shader = self.v_shader;
-		let f_shader = self.f_shader;
-		group.execute(move |core, rm| {
-			core.attach_shader_to_pipeline(pipeline, rm.shaders.get_name(v_shader).unwrap());
-			core.attach_shader_to_pipeline(pipeline, rm.shaders.get_name(f_shader).unwrap());
-
-			unsafe {
-				core.gl.BindProgramPipeline(pipeline.0);
-				core.gl.DrawArrays(gl::POINTS, 0, 10);
-			}
-
-			core.debug_marker("User Callback");
-		});
+		group.draw(self.v_shader, self.f_shader)
+			.primitive(gfx::command::draw::PrimitiveType::Points)
+			.elements(10);
 	}
 }
