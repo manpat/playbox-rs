1c6c64b2aa7a3952e30f44ab8f16988fe85d41dd
Patrick Monaghan <0x5f.manpat@gmail.com>
2023-08-06 19:42:29 +0100

    binding ergonomics - perspective rendering

--- resource/shaders/test.vs.glsl
+++ resource/shaders/test.vs.glsl
@@ -12,9 +12,13 @@ layout(binding=0) uniform U {
 	float u_time_sin;
 };
 
+layout(binding=1) uniform P {
+	mat4 u_projection_view;
+};
+
 void main() {
 	float offset = gl_InstanceID/10.0;
-	gl_Position = vec4(points[gl_VertexID % 3] + vec2(sin(u_time_sin - offset*2.0), offset-0.2), offset, 1.0);
+	gl_Position = u_projection_view * vec4(points[gl_VertexID % 3] + vec2(sin(u_time_sin - offset*2.0), offset-0.2), offset, 1.0);
 	gl_PointSize = 10.0;
 
 	v_instance = float(gl_InstanceID) / 10.0;
--- src/main.rs
+++ src/main.rs
@@ -35,25 +35,39 @@ impl App {
 
 impl toybox::App for App {
 	fn present(&mut self, ctx: &mut toybox::Context) {
+		let aspect = 1.0;
+		let projection = Mat4::perspective(PI/3.0, aspect, 0.01, 100.0)
+			* Mat4::translate(Vec3::from_z(-3.0))
+			* Mat4::rotate_x(PI/16.0)
+			* Mat4::rotate_y(PI/6.0 + self.time/3.0);
+
+		let projection_upload = ctx.gfx.frame_encoder.upload(&projection);
+
 		let mut group = ctx.gfx.frame_encoder.command_group("MY Group");
 		group.debug_marker("Group Time");
 
 		self.time += 1.0/60.0;
 
-		group.upload_stage.stage_data(&[1.0f32, 2.0, 3.0]);
-		let upload_id = group.upload_stage.stage_data(&[self.time]);
-		group.upload_stage.stage_data(&[1.0f32, 2.0, 3.0]);
+		let upload_id = group.upload(&self.time);
+		
+		group.draw(self.v_shader, self.f_shader)
+			.primitive(gfx::command::draw::PrimitiveType::Triangles)
+			.elements(3)
+			.ubo(0, &0.0f32)
+			.ubo(1, projection_upload);
 		
 		group.draw(self.v_shader, self.f_shader)
 			.primitive(gfx::command::draw::PrimitiveType::Triangles)
 			.elements(3)
 			.instances(8)
-			.ubo(0, upload_id);
+			.ubo(0, upload_id)
+			.ubo(1, projection_upload);
 
 		group.draw(self.v_shader, self.f_shader)
 			.primitive(gfx::command::draw::PrimitiveType::Points)
 			.elements(10)
-			.ubo(0, upload_id);
+			.ubo(0, &(self.time*2.0))
+			.ubo(1, projection_upload);
 
 	}
 }
