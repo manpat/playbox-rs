912bf1ff8f09b68175c37097698db83d386005e9
Patrick Monaghan <0x5f.manpat@gmail.com>
2023-08-20 01:23:15 +0100

    test audio provider

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,8 @@ impl App {
 	fn new(ctx: &mut toybox::Context) -> anyhow::Result<App> {
 		ctx.gfx.frame_encoder.backbuffer_color([1.0, 0.5, 1.0]);
 
+		ctx.audio.set_provider(MyAudioProvider::default())?;
+
 		let mut group = ctx.gfx.frame_encoder.command_group("START");
 		group.debug_marker("FUCK");
 
@@ -35,7 +37,7 @@ impl App {
 
 impl toybox::App for App {
 	fn present(&mut self, ctx: &mut toybox::Context) {
-		let aspect = 1.0;
+		let aspect = ctx.gfx.backbuffer_aspect();
 		let projection = Mat4::perspective(PI/3.0, aspect, 0.01, 100.0)
 			* Mat4::translate(Vec3::from_z(-3.0))
 			* Mat4::rotate_x(PI/16.0)
@@ -69,5 +71,33 @@ impl toybox::App for App {
 			.ubo(0, &(self.time*2.0))
 			.ubo(1, projection_upload);
 
+	}
+}
+
+
+#[derive(Default)]
+struct MyAudioProvider {
+	sample_dt: f32,
+	phase: f32,
+}
+
+impl audio::Provider for MyAudioProvider {
+	fn on_configuration_changed(&mut self, config: audio::Configuration) {
+		self.sample_dt = 1.0/config.sample_rate;
+		assert!(config.channels == 2);
+	}
+
+	fn fill_buffer(&mut self, buffer: &mut [f32]) {
+		for frame in buffer.chunks_exact_mut(2) {
+			let osc = (self.phase * 110.0 * TAU).sin() * 0.1;
+			let amp = 0.5 - self.phase.cos() * 0.5;
+			let amp = amp * amp;
+
+			frame[0] = osc * amp;
+			frame[1] = -osc * amp;
+			self.phase += self.sample_dt;
+		}
+
+		self.phase %= 2.0 * PI;
 	}
 }
