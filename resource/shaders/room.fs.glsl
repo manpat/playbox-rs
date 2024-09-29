8ba73f3da755925922337fa93039f5a7612c0801
Patrick Monaghan <0x5f.manpat@gmail.com>
2024-09-29 19:39:57 +0100

    Add missing file

---
+++ resource/shaders/room.fs.glsl
@@ -0,0 +1,13 @@
+
+in Vertex {
+	vec4 v_color;
+	vec2 v_uv;
+};
+
+out vec4 o_color;
+
+layout(binding=0) uniform sampler2D u_texture;
+
+void main() {
+	o_color = texture(u_texture, v_uv*8.0) * v_color;
+}
